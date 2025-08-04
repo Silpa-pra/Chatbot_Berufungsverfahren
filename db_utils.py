7b984b31f94958b2b91add9f639921be857a643f
359146 <silpa.prajapati@stud.th-luebeck.de>
2025-08-04 18:21:00 +0200

    updated with context manager

--- db_utils.py
+++ db_utils.py
@@ -2,6 +2,7 @@ import mysql.connector
 from urllib.parse import quote_plus
 from dotenv import load_dotenv
 import os
+from contextlib import contextmanager
 
 load_dotenv()
 
@@ -14,3 +15,17 @@ def get_db_connection():
         database = os.getenv("DB_NAME")
     )
     return conn
+
+@contextmanager
+def get_db_cursor(dictionary = True):
+    """Context manager for database operations"""
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=dictionary)
+    try:
+        yield conn, cursor
+    except Exception as e:
+        conn.rollback()
+        raise e
+    finally:
+        cursor.close()
+        conn.close()
